0caf029edc17b57a10e340e1fa44a10282c19b82
SUA GARCIA JHOAN SEBASTIAN <140029785+ZeB-ZuA@users.noreply.github.com>
2024-04-18 16:08:31 -0500

    Update AutoVentaDB.sql
    Correccion del script

--- AutoVentaDB.sql
+++ AutoVentaDB.sql
@@ -2,18 +2,19 @@
 
 DROP DATABASE IF EXISTS AutoVentaDB;
 
-CREATE DATABASE AutoVenta;
+CREATE DATABASE AutoVentaDB;
 
-USE AutoVenta;
+USE AutoVentaDB;
 
 -- user table
 CREATE TABLE Usuario (
-    ID_Usuario INT AUTO_INCREMENT,
+    ID_Usuario INT,
     Cedula LONG,
     Correo_Electronico VARCHAR(255),
     Contraseña VARCHAR(255),
     Nombre VARCHAR(255),
     Apellido VARCHAR(255),
+    credito LONG,
     Fecha_Registro DATETIME
 );
 
@@ -31,7 +32,8 @@ CREATE TABLE Usuario_Rol(
 
 -- Addinng primary keys
 
-ALTER TABLE Usuario ADD PRIMARY KEY (ID_Usuario);
+ALTER TABLE Usuario MODIFY COLUMN ID_Usuario INT AUTO_INCREMENT,
+ADD PRIMARY KEY (ID_Usuario);
 ALTER TABLE Rol ADD PRIMARY KEY (ID_Rol);
 ALTER TABLE Usuario_Rol ADD PRIMARY KEY (ID_Usuario, ID_Rol); -- check
 
@@ -39,7 +41,8 @@ ALTER TABLE Usuario_Rol ADD PRIMARY KEY (ID_Usuario, ID_Rol); -- check
 -- Adding foreign keys
 
 ALTER TABLE Usuario_Rol ADD FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario) ON DELETE CASCADE;
-ALTER TABLE Usuario_Rol ADD FOREIGN KEY (ID_Rol) REFERENCES Usuario(ID_Rol) ON DELETE CASCADE;
+ALTER TABLE Usuario_Rol ADD FOREIGN KEY (ID_Rol) REFERENCES Rol(ID_Rol) ON DELETE CASCADE;
+
 
 -- Adding constraints
 ALTER TABLE Usuario ADD UNIQUE (Correo_Electronico);
