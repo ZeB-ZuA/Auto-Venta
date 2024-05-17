a4e042cabc586ca953771ebd012202a89cbcef58
Chocolxt <dhsepulvedas@udistrital.edu.co>
2024-05-16 20:34:42 -0500

    Frotend initialization

---
+++ App/views/features.php
@@ -0,0 +1,30 @@
+<section class="container container-features">
+    <div class="card-feature">
+        <i class="fa-solid fa-plane-up"></i>
+        <div class="feature-content">
+            <span>Envío a cualquier lugar</span>
+            <p>Solo pedidos nacionales</p>
+        </div>
+    </div>
+    <div class="card-feature">
+        <i class="fa-solid fa-wallet"></i>
+        <div class="feature-content">
+            <span>Contrareembolso</span>
+            <p>100% garantía de devolución de dinero</p>
+        </div>
+    </div>
+    <div class="card-feature">
+        <i class="fa-regular fa-thumbs-up"></i>
+        <div class="feature-content">
+            <span>Productos Certificados</span>
+            <p>Garantizamos la calidad de tu producto</p>
+        </div>
+    </div>
+    <div class="card-feature">
+        <i class="fa-solid fa-headset"></i>
+        <div class="feature-content">
+            <span>Servicio al cliente 24/7</span>
+            <p>LLámenos 24/7 al 123-456-7890</p>
+        </div>
+    </div>
+</section>
---
+++ App/views/suppliers.php
@@ -0,0 +1,18 @@
+
+<section class="container suppliers">
+    <h1 class="heading-1">Nuestros proveedores</h1>
+    <div class="container-suppliers">
+        <div class="card-suppliers suppliers-1">
+            <p>Proveedor 1</p>
+            <span>Ver más</span>
+        </div>
+        <div class="card-suppliers suppliers-2">
+            <p>Proveedor 2</p>
+            <span>Ver más</span>
+        </div>
+        <div class="card-suppliers suppliers-3">
+            <p>Proveedor 3</p>
+            <span>Ver más</span>
+        </div>
+    </div>
+</section>
