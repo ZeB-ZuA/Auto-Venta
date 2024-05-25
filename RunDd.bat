b33444aff7020a61702551718e8ee4b1cb3d4713
SUA GARCIA JHOAN SEBASTIAN <140029785+ZeB-ZuA@users.noreply.github.com>
2024-05-25 18:22:20 -0500

    Update RunDd.bat

--- RunDd.bat
+++ RunDd.bat
@@ -3,7 +3,7 @@ set /p user="Usuario de MySQL: "
 set /p password="Contrasenia de MySQL: "
 set /p port="Puerto de MySQL: "
 
-mysql -u %user% -p%password% -P%port% < ./ScriptEcofertilDB.sql
+mysql -u %user% -p%password% -P%port% < ./AutoVentaDB.sql
 echo Cargando base de datos...
 timeout /t 3 /nobreak > NUL
 echo Base de datos cargada.
