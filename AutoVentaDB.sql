434f999c8e9a671e68a068ac24157d23e1575007
SUA GARCIA JHOAN SEBASTIAN <140029785+ZeB-ZuA@users.noreply.github.com>
2024-05-26 17:20:34 -0500

    Update AutoVentaDB.sql

--- AutoVentaDB.sql
+++ AutoVentaDB.sql
@@ -71,9 +71,11 @@ VALUES (20212578104, 'DannaVendedora@danna.com', '123456', 'Danna', 'Sepulveda',
 
 INSERT INTO Car (ID_Seller, Plate, Brand, Model, Year, Color, Doors, Cc, Transmission, Fuel_Type, Used, Kilometers, Image, Price, status)
 VALUES 
-(1, 'ABC1234', 'Toyota', 'Corolla', 2019, 'Red', 4, 1800, 'Automatic', 'Gasoline', 'No', 15000, 'car1.jpg', 15000, 'Available'),
-(1, 'XYZ5678', 'Honda', 'Civic', 2020, 'Blue', 4, 2000, 'Manual', 'Diesel', 'No', 10000, 'car2.jpg', 18000, 'Available'),
-(1, 'DEF9012', 'Ford', 'Fiesta', 2018, 'White', 4, 1600, 'Automatic', 'Gasoline', 'Yes', 30000, 'car3.jpg', 12000, 'Available');
+(1, 'ABC1234', 'Toyota', 'Corolla', 2019, 'Rojo', 4, 1800, 'Automatico', 'Gasolina', 'No', 15000, '../uploads/toyotarojo.jpg', 15000, 'Available'),
+(1, 'XYZ5678', 'Honda', 'Civic', 2020, 'Azul', 4, 2000, 'Manual', 'Diesel', 'No', 10000, '../uploads/hondaCivic.jpg', 18000, 'Available'),
+(1, 'DEF9012', 'Ford', 'Fiesta', 2018, 'Blanco', 4, 1600, 'Automatico', 'Gasolina', 'si', 30000, '../uploads/fordBlanco.jpg', 12000, 'Available'),
+(1, 'DEF9013', 'Subaru', 'Impreza', 2023, 'Gris', 4, 1300, 'Manual', 'Gasolina', 'si', 80000, '../uploads/fordBlanco.jpg', 20000, 'Available')
+
 
 
 
